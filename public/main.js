5da9b3d88d474f9a24d4beed9040089d653002ae
seasouth <csouthern333@gmail.com>
2015-11-07 17:37:34 -0500

    Update main.js

--- public/main.js
+++ public/main.js
@@ -2,18 +2,18 @@ $(document).ready(function(){
 
 $(function (){
   
-  var $cc-number = $('#cc-number');
-  var $cc-cvc = $('#cc-cvc');
-  var $cc-exp-month = $('#cc-exp-month');
-  var $cc-exp-year = $('#cc-exp-year');
-});
+  var $number = $('#cc-number');
+  var $cvc = $('#cc-cvc');
+  var $expMonth = $('#cc-exp-month');
+  var $expYear = $('#cc-exp-year');
+
   
   $('#process-payment-btn').on('click', funtion() {
     var info = {
-      cc-number: $cc-number.val();
-      cc-cvc: $cc-cvc.val();
-      cc-exp-month: $cc-exp-month();
-      cc-exp-year: $cc-exp-year();
+      cc-number: $cc-number.val(),
+      cc-cvc: $cc-cvc.val(),
+      cc-exp-month: $cc-exp-month.val(),
+      cc-exp-year: $cc-exp-year.val(),
       };
       
     $.ajax({
