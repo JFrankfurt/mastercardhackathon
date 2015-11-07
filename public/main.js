c3590c641b89063165e6108699fab77eebe36eb6
seasouth <csouthern333@gmail.com>
2015-11-07 17:25:26 -0500

    Update main.js

--- public/main.js
+++ public/main.js
@@ -6,5 +6,21 @@ $(function (){
   var $cc-cvc = $('#cc-cvc');
   var $cc-exp-month = $('#cc-exp-month');
   var $cc-exp-year = $('#cc-exp-year');
+});
+  
+  $('#process-payment-btn').on('click', funtion() {
+    var info = {
+      cc-number: $cc-number.val();
+      cc-cvc: $cc-cvc.val();
+      cc-exp-month: $cc-exp-month();
+      cc-exp-year: $cc-exp-year();
+      };
+      
+    $.ajax({
+      type: 'POST',
+      url: 'https://serene-inlet-7047.herokuapp.com/transaction/:amount/:description/:expMonth/:expYear/:cvc/:number/:currency',
+      data: info,
+      });
+  });
   
 });
