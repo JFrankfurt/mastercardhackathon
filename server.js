945a5073e6e0af259b6ec666c49ba52177fc910e
Jordan Frankfurt <generalj10@gmail.com>
2015-11-07 17:27:03 -0500

    cole did some jquery stuff

--- server.js
+++ server.js
@@ -9,10 +9,26 @@ var Simplify = require('simplify-commerce'),
         publicKey: 'sbpb_MTI2YmUwOTYtNDBjZi00ZTUxLTgxYzctMTIxZGEwMjc5OTEx',
         privateKey: 'wveDFv/Gi8MbNvlQozacvBShSLFw+TD+joMdJh2+hUd5YFFQL0ODSXAOkNtXTToq'
     });
-
-var localDiscountRate = 0.05,
-    subsidy = 0,
-    newBill = 0;
+/*
+function GovCoupon () {
+    var localDiscountRate = 5,
+        subsidy = 0,
+        newBill = 0;
+    client.coupon.create({
+        startDate: new Date() - 100,
+        description: "Local customer subsidy",
+        maxRedemptions: new Date() + 1,
+        couponCode: "local",
+        endDate: new Date() + 100,
+        percentOff: localDiscountRate
+    }, function (errData, data) {
+        if(errData){
+            console.error("Error Message: " + errData.data.error.message);
+            // handle the error
+            return;
+    } console.log("Success Response: " + JSON.stringify(data));
+});
+*/
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -22,11 +38,51 @@ app.get('/', function (req, res){
     res.render('public/index.html');
 });
 
+app.post('/vendorinfo/:zip', function (req, res) {
+    vendorzip = req.params.zip;
+});
+
 app.post('/transaction/:amount/:description/:expMonth/:expYear/:cvc/:number/:currency', function (req, res, next) {
     if (true){
+        var localDiscountRate = 0.05;
         subsidy = req.params.amount * localDiscountRate;
         newBill = req.params.amount - subsidy;
 
+        client.payment.create({
+            amount: newBill,
+            desciption: "New bill to customer with local discount",
+            card: {
+                expMonth: req.params.expMonth,
+                expYear: req.params.expYear,
+                cvc: req.params.cvc,
+                number: req.params.number
+            },
+            currency: req.params.currency
+        }, function (errData, data) {
+            if (errData) {
+                console.error("Error message: " + errData.data.error.message);
+                return;
+            }
+            console.log("Payment status: " + data.paymentStatus);
+        });
+
+        client.payment.create({
+            amount: subsidy,
+            desciption: "Payment from gov't to business for local discount.",
+            card: {
+                expMonth: req.params.expMonth,
+                expYear: req.params.expYear,
+                cvc: req.params.cvc,
+                number: req.params.number
+            },
+            currency: req.params.currency
+        }, function (errData, data) {
+            if (errData) {
+                console.error("Error message: " + errData.data.error.message);
+                return;
+            }
+            console.log("Payment status: " + data.paymentStatus);
+        });
         //some stuff
         next();
     } else {
@@ -47,6 +103,7 @@ app.post('/transaction/:amount/:description/:expMonth/:expYear/:cvc/:number/:cur
             }
             console.log("Payment status: " + data.paymentStatus);
         });
+        next();
     }
 });
 
